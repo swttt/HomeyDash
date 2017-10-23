cdb72527e2a458a33d92cff22ae1ff1623cb920e
Bas Jansen <b@sjansen.email>
2017-10-23 19:25:26 +0200

    cordova

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // === DEFAULT / CUSTOM STYLE ===
 // WARNING! always comment out ONE of the two require() calls below.
 // 1. use next line to activate CUSTOM STYLE (./src/themes)
-require(`./themes/app.${__THEME}.styl`)
+require(`./themes/app.ios.styl`)
 // 2. or, use next line to activate DEFAULT QUASAR STYLE
 // require(`quasar/dist/quasar.${__THEME}.css`)
 // ==============================
