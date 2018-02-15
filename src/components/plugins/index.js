6c764a103134b5e9034d9d1d8ffa563ad3206b4e
Bas Jansen <b@sjansen.email>
2018-02-15 01:38:57 +0100

    Added a scaffolding tool

--- src/components/plugins/index.js
+++ src/components/plugins/index.js
@@ -1,12 +1,13 @@
 // IMPORT PLUGIN FOLDERS
+// VAR SHOULD BE THE SAME AS YOUR ID
 import powerUsage from './power-usage/'; // USE YOUR PLUGIN ID
 import devices from './devices/';
 import alarm from './alarm/';
 
 
+// EXPORT YOUR PLUGIN HERE
 export default {
   devices,
   alarm,
   powerUsage
-
 }
