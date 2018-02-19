18ef7a98bb86a7f0509be0f72e6ffc57d686e5e2
Bas Jansen <b@sjansen.email>
2018-02-19 23:49:18 +0100

    Added date

--- src/components/widget-system/widgets/clockhd/Main.vue
+++ src/components/widget-system/widgets/clockhd/Main.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-  <h1>{{time}}</h1>
+  <h1>{{time}}<br/><small>{{day}}</small></h1>
 </div>
 </template>
 
@@ -12,6 +12,7 @@ export default {
   data() {
     return {
       time:'',
+      day: '',
       interval: null
     }
   },
@@ -23,6 +24,7 @@ export default {
     getTime(){
       this.interval = setTimeout(() => {
         this.time = moment().format('H:mm:ss');
+        this.day = moment().format('D. MMMM YYYY');
         this.getTime();
     }, 1000);
     }
@@ -39,7 +41,15 @@ export default {
 h1
   margin 0px
   padding-left 15px
+  padding-top 30px
+  padding-bottom 15px
   width 100%
   text-align left
+  line-height 30px
+
+  small
+    font-size 30px
+    line-height 30px
+    padding-left 35px
 
 </style>
