fb16a933046cfa971910e24fe77356cc4e843bc4
jghaanstra <jghaanstra@gmail.com>
2018-02-20 20:05:04 +0100

    IP Webcam Widget

---
+++ src/components/widget-system/widgets/ipwebcam/Main.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="ipwebcam">
+        <img v-bind:src="widget.settings.url" alt="IP Webcam" border="0" />
+        <div class="name" v-if="widget.settings.showname">{{ widget.settings.name }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['widget']
+    }
+</script>
+
+<style lang="stylus" scoped>
+    @import '~variables'
+
+    .ipwebcam img {
+        width: 100%;
+        height: auto;
+    }
+    .ipwebcam .name {
+        display: block
+        width: 100%;
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        padding: 8px;
+        color: #fff;
+        font-size: 12px;
+        background-color: rgba(0, 0, 0, 0.5);
+    }
+</style>
---
+++ src/components/widget-system/widgets/ipwebcam/Settings.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-12">
+        <q-input dark v-model="widget.settings.name" stack-label="Name" />
+        <q-input dark v-model="widget.settings.url" stack-label="Stream URL" />
+        <q-toggle dark v-model="widget.settings.showname" label="Show name as overlay?" />
+        <q-input dark v-model="widget.width" stack-label="Width" />
+        <q-input dark v-model="widget.height" label="Height" />
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ['widget'],
+        data() {
+            return {
+
+            }
+        },
+        created(){
+            this.widget.settings = {};
+        }
+    }
+</script>
