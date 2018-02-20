242ef7d563b9a5f6c6da98a163327f332378231e
Bas Jansen <b@sjansen.email>
2018-02-20 22:15:34 +0100

    Made text area smaller

--- src/components/widget-system/widgets/simplebox/Settings.vue
+++ src/components/widget-system/widgets/simplebox/Settings.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="col-12">
-  <q-input v-model="widget.settings.text" type="textarea" float-label="Box content" :max-height="100" :min-rows="7" dark></q-input>
+  <q-input v-model="widget.settings.text" type="textarea" float-label="Box content" :min-rows="2" dark></q-input>
 </div>
 </template>
 
