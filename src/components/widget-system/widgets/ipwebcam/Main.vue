d79c24129effe1566f0f766a017e8bc58d46d52a
jghaanstra <jghaanstra@gmail.com>
2018-02-21 23:35:39 +0100

    IP Webcam widget

--- src/components/widget-system/widgets/ipwebcam/Main.vue
+++ src/components/widget-system/widgets/ipwebcam/Main.vue
@@ -1,13 +1,96 @@
 <template>
     <div class="ipwebcam">
-        <img v-bind:src="widget.settings.url" alt="IP Webcam" border="0" />
+        <img id="stream" v-bind:src="webcamUrl()" v-bind:alt="widget.settings.name" border="0" onLoad="webcamUrl()" />
         <div class="name" v-if="widget.settings.showname">{{ widget.settings.name }}</div>
     </div>
 </template>
 
 <script>
     export default {
-        props: ['widget']
+        props: ['widget'],
+        methods: {
+            webcamUrl: function () {
+                var url = '';
+                if (this.widget.settings.secure) {
+                    url = 'https://'+ this.widget.settings.url;
+                } else {
+                    url = 'http://'+ this.widget.settings.url;
+                }
+                /*if (this.widget.settings.username && this.widget.settings.password) {
+                    console.log('using credentials');
+                    var options = {
+                        url: url,
+                        method: 'GET',
+                        headers: {
+                            'Authorization': "Basic " + btoa(this.widget.settings.username + ":" + this.widget.settings.password),
+                            'Access-Control-Allow-Origin':  'http://homeydash.local:8081',
+                            'Access-Control-Allow-Methods': 'GET',
+                            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                            'Access-Control-Allow-Credentials': 'true'
+                        }
+                    }
+                } else {
+                    console.log('not using credentials');
+                    var options = {
+                        url: url,
+                        method: 'GET',
+                        headers: {
+                            'Access-Control-Allow-Origin':  'http://homeydash.local:8081',
+                            'Access-Control-Allow-Methods': 'GET',
+                            'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                            'Access-Control-Allow-Credentials': 'true'
+                        }
+                    }
+                }
+
+                this.$http(options)
+                    .then((response) => {
+                        console.log('success');
+                        return response.body;
+                    }, (error) => { console.log('whoops'); console.log(error); });*/
+
+                /*this.axios.defaults.withCredentials = true;
+
+                let axiosConfig = {
+                    headers: {
+                        'crossdomain': 'true',
+                        'Access-Control-Allow-Origin':  'http://homeydash.local:8081',
+                        'Access-Control-Allow-Methods': 'GET',
+                        'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+                        'Access-Control-Allow-Credentials': true
+                    }
+                };
+
+                this.axios.get(url)
+                    .then((response) => {
+                      console.log("RESPONSE RECEIVED: ", response);
+                      return response.body;
+                    })
+                    .catch((error) => {
+                      console.log("AXIOS ERROR: ", error);
+                  })*/
+
+                fetch(url, {
+                       method: 'GET',
+                       mode: 'no-cors',
+                       credentials: 'same-origin',
+                       headers: {
+                           'Authorization': "Basic " + btoa(this.widget.settings.username + ":" + this.widget.settings.password),
+                           'Access-Control-Allow-Origin':  'http://homeydash.local:8081',
+                           'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
+                           'Access-Control-Allow-Credentials': true
+                       }
+                   }
+                )
+                    .then(response => response.json())
+                    .then(json => {
+                        return json.body;
+                    })
+                    .catch(function(error) {
+                        console.log(error);
+                    });
+            }
+        }
     }
 </script>
 
--- src/components/widget-system/widgets/ipwebcam/Settings.vue
+++ src/components/widget-system/widgets/ipwebcam/Settings.vue
@@ -1,10 +1,13 @@
 <template>
     <div class="col-12">
-        <q-input dark v-model="widget.settings.name" stack-label="Name" />
-        <q-input dark v-model="widget.settings.url" stack-label="Stream URL" />
-        <q-toggle dark v-model="widget.settings.showname" label="Show name as overlay?" />
-        <q-input dark v-model="widget.width" stack-label="Width" />
-        <q-input dark v-model="widget.height" label="Height" />
+        <q-input dark v-model="widget.settings.name" type="text" stack-label="Name" />
+        <q-toggle dark v-model="showName" label="Show name as overlay?" />
+        <q-input dark v-model="widget.settings.url" type="text" stack-label="Stream URL (without http)" />
+        <q-toggle dark v-model="useHttps" label="Use HTTPS?" />
+        <q-input dark v-model="widget.settings.username" type="text" stack-label="HTTP authentication username" />
+        <q-input dark v-model="widget.settings.password" type="password" stack-label="HTTP authentication password" />
+        <q-input dark v-model="widget.width" type="number" stack-label="Width" />
+        <q-input dark v-model="widget.height" type="number" label="Height" />
     </div>
 </template>
 
@@ -13,11 +16,24 @@
         props: ['widget'],
         data() {
             return {
-
+                showName: false,
+                useHttps: false,
             }
         },
         created(){
-            this.widget.settings = {};
+            if (!this.widget.settings)
+                this.widget.settings = {};
+            else
+                this.showName = this.widget.settings.showname;
+                this.useHttps = this.widget.settings.secure;
+        },
+        watch: {
+            showName: function (newVal) {
+                this.widget.settings.showname = newVal;
+            },
+            useHttps: function (newVal) {
+                this.widget.settings.secure = newVal;
+            },
         }
     }
 </script>
