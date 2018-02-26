efea4722a3aa7aa5ae8197afc10323348c7b2865
Bas Jansen <b@sjansen.email>
2018-02-26 19:33:03 +0100

    Add remove local storage button

--- src/components/base/components/Settings.vue
+++ src/components/base/components/Settings.vue
@@ -30,13 +30,13 @@
               <q-tab-pane name="general">
                 <q-item class="subhead text-teal">
                   Plugins
-                  </q-item>
+                </q-item>
                 <q-item v-for="plugin in plugins" :key="plugin.id">
                   <q-item-main>
                     <q-item-tile label>{{plugin.name}}</q-item-tile>
                   </q-item-main>
                   <q-item-side right>
-                    <q-toggle  v-model="settings.plugins[plugin.id].enabled" color="teal" />
+                    <q-toggle v-model="settings.plugins[plugin.id].enabled" color="teal" />
                   </q-item-side>
                 </q-item>
               </q-tab-pane>
@@ -52,21 +52,30 @@
               <q-tab-pane name="info">
                 <q-item class="subhead text-teal">
                   Patreon
-                  </q-item>
-                <q-item >
+                </q-item>
+                <q-item>
                   <q-item-main>
-                  If you like my project, please consider becoming a Patron! With your support i can keep the server and coffee running.
-                  <br/><br/>
-                  <center><a href="https://www.patreon.com/bePatron?u=9768305" target="_blank" class="text-teal" style="font-size:20px;">Become a Patron now!</a></center>
-                  <br/>
+                    If you like my project, please consider becoming a Patron! With your support i can keep the server and coffee running.
+                    <br/><br/>
+                    <center><a href="https://www.patreon.com/bePatron?u=9768305" target="_blank" class="text-teal" style="font-size:20px;">Become a Patron now!</a></center>
+                    <br/>
                   </q-item-main>
                 </q-item>
                 <q-item class="subhead text-teal">
                   Privacy & Cookies
-                  </q-item>
-                  <q-item>
-                    HomeyDash is using analytic cookies to see how the dashboard is used and where it could improve. I do care about your privacy, so no personal information is stored. For that same reason i am not using Google analytics but Matomo.
-                    </q-item>
+                </q-item>
+                <q-item>
+                  HomeyDash is using analytic cookies to see how the dashboard is used and where it could improve. I do care about your privacy, so no personal information is stored. For that same reason i am not using Google analytics but Matomo.
+                </q-item>
+                <q-item class="subhead text-red">
+                  Danger Zone
+                </q-item>
+                <q-item>
+                  Only use the remove local storage button if you are 100% sure you want to clear all settings.
+                </q-item>
+                <q-item>
+                  <q-btn color="red" style="width: 100%;" v-on:click="clearStorage()" icon="delete">REMOVE LOCAL STORAGE</q-btn>
+                </q-item>
               </q-tab-pane>
 
             </q-tabs>
@@ -96,7 +105,7 @@ export default {
       plugins: plugins
     }
   },
-  components:{
+  components: {
     plugins
   },
   async mounted() {
@@ -122,6 +131,13 @@ export default {
     async saveSettings() {
       await this.$store.commit('updateSettings', this.settings)
       location.reload();
+    },
+    clearStorage() {
+        localStorage.removeItem('vuex');
+        setTimeout(() => {
+          location.reload();
+        }, 1000);
+
     }
   },
   computed: {
@@ -143,6 +159,7 @@ export default {
 
 .settings-content
   color white
+  margin-bottom 50px
 
 .q-list
   border 0px!important
