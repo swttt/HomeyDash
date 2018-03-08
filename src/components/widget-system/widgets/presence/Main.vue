13ac4dab7212fff0d90a4ffe166ac2ea4df9ff98
D Schaaf <dschaaf@MacBook-Pro-van-D.local>
2018-03-08 01:01:01 +0100

    presence

---
+++ src/components/widget-system/widgets/presence/Main.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+  <q-list v-if="!loading">
+
+    <q-item v-for="user in widget.settings.users" :key="user">
+      <q-item-main>
+        {{widget.settings.homeyUsers[user].name}}
+        </q-item-main>
+      <q-item-side right>
+      {{widget.settings.homeyUsers[user].icon}}
+      </q-item-side>
+    </q-item>
+  </q-list>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['widget'],
+  data() {
+    return {
+      users: {},
+      loading: true
+    }
+  },
+  async mounted() {
+    await this.loopUsers();
+    this.loading = false;
+  },
+  methods: {
+    async loopUsers() {
+      this.widget.settings.users.forEach(async (user) => {
+        this.widget.settings.homeyUsers[user] = await this.$homey.users.getUser({
+          id: user
+        });
+        await this.$homey.users.subscribe();
+        this.widget.settings.homeyUsers[user].on('$state', state => {
+           console.log(state);
+        });
+      })
+    },
+
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~variables'
+
+h5
+  margin 0px
+  padding 0px
+
+</style>
