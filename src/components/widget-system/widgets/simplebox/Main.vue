e5f170f368694d11629f777129e7ad2a0144b3b4
3vad <33979136+3vad@users.noreply.github.com>
2018-03-11 22:30:25 +0100

    Update Main.vue

--- src/components/widget-system/widgets/simplebox/Main.vue
+++ src/components/widget-system/widgets/simplebox/Main.vue
@@ -1,9 +1,9 @@
 <template>
 <div class="content" v-if="widget.settings.centered">
-  <q-item-title class="text-white"><center>{{widget.settings.text}}</center></q-item-title>
+  <q-item-tile class="text-white"><center>{{widget.settings.text}}</center></q-item-tile>
   </div>
 <div class="content" v-else>
-  <q-item-title class="text-white">{{widget.settings.text}}</q-item-title>
+  <q-item-tile class="text-white">{{widget.settings.text}}</q-item-tile>
 </div>
 </template>
 
