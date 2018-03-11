2feec3c8e7738c19b28bc5f2816340830e1a7e79
Bas Jansen <b@sjansen.email>
2018-03-11 07:39:40 +0100

    Fix ip camera widget settings

--- src/components/widget-system/widgets/ipwebcam/Settings.vue
+++ src/components/widget-system/widgets/ipwebcam/Settings.vue
@@ -1,9 +1,28 @@
 <template>
     <div class="col-12">
-        <q-input dark v-model="widget.settings.name" type="text" stack-label="Name" />
-        <q-toggle dark v-model="widget.settings.showName" label="Show name as overlay?" />
-        <q-input dark v-model="widget.settings.url" type="text" stack-label="Stream URL" />
-        <q-input dark v-model="widget.width" type="number" stack-label="Widget Width" />
+      <q-list no-border>
+      <q-item tag="label">
+        <q-item-main>
+          <q-input color="teal" dark v-model="widget.settings.name" type="text" float-label="Name" />
+        </q-item-main>
+      </q-item>
+      <q-item tag="label">
+        <q-item-main label="Show name as overlay" />
+        <q-item-side right>
+          <q-toggle color="teal" dark v-model="widget.settings.showName" />
+        </q-item-side>
+      </q-item>
+      <q-item tag="label">
+        <q-item-main>
+          <q-input dark color="teal" v-model="widget.settings.url" type="text" float-label="Stream URL" />
+        </q-item-main>
+      </q-item>
+      <q-item tag="label">
+        <q-item-main>
+          <q-input dark color="teal" v-model="widget.width" type="number" float-label="Widget Width" />
+        </q-item-main>
+      </q-item>
+    </q-list>
     </div>
 </template>
 
