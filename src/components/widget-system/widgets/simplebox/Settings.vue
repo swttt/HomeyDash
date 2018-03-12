aa1d4abe71aaf9ea692bd05a60ce871149008f32
3vad <33979136+3vad@users.noreply.github.com>
2018-03-12 14:11:44 +0100

    enhancement

--- src/components/widget-system/widgets/simplebox/Settings.vue
+++ src/components/widget-system/widgets/simplebox/Settings.vue
@@ -1,6 +1,24 @@
 <template>
 <div class="col-12">
-  <q-input v-model="widget.settings.text" type="textarea" float-label="Box content" :min-rows="2" dark></q-input>
+<q-item tag="label">
+  <q-item-main>
+  <q-input dark color="teal" v-model="widget.settings.text" type="textarea" float-label="Box content" :min-rows="2"></q-input>
+  </q-item-main>
+</q-item>
+  <q-item tag="label">
+    <q-item-main>
+      <q-input dark color="teal" v-model="widget.width" type="number" float-label="Widget Width" />
+    </q-item-main>
+  </q-item>
+  <q-list-header>Details to show</q-list-header>
+  <q-item tag="label">
+  <q-item-main>
+  <q-item-tile label>Show text centered</q-item-tile>
+  </q-item-main>
+      <q-item-side right>
+          <q-toggle dark v-model="widget.settings.centered" color="teal" />
+      </q-item-side>
+    </q-item>
 </div>
 </template>
 
